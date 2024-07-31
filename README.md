773ef30d9d385bafa8b009767494450a235d8f15
drflamemontgomery <drflamemontgomery@gmail.com>
2024-08-01 11:00:50 +1200

    Forked from DyslexicReader

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Dyslexic Bible
+
+The dyslexic Bible is an application targeted towards people with dyslexia. It aims to improve the experience for dyslexics so that they can distinguish words from each other.
---
+++ src/main.zig
@@ -0,0 +1,43 @@
+const glfw = @import("glfw");
+const std = @import("std");
+const ui = @import("ui/ui.zig");
+const Window = @import("Window.zig");
+const ScaledFont = @import("graphics.zig").ScaledFont;
+
+pub fn main() !void {
+    // Setup an ArenaAllocator so we can deallocate everything afterwards
+    var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_state.deinit();
+    const arena = arena_state.allocator();
+
+    // Init our ScaledFont to handle our fonts and sizes.
+    ScaledFont.init(arena);
+    defer ScaledFont.deinit();
+
+    // Initialize Our window context
+    try Window.init();
+    defer Window.terminate();
+
+    var window = try Window.new(arena, "Dyslexic Reader", 1024, 768);
+    defer window.destroy();
+
+    // resize our window to build our context
+    try window.resize(1024, 768);
+
+    Window.current = &window;
+
+    try mainLoop(arena, &window);
+}
+
+fn mainLoop(allocator: std.mem.Allocator, window: *Window) !void {
+    var text = try ui.Text.new(allocator, "Hello World!", .{});
+    text.color = ui.Color.fromHSV(50, 0.79, 0.8);
+    text.color.printHex();
+
+    // Create the Text Component with the Window Component as a parent
+    _ = try text.getComponent(allocator, &window.ctx.component);
+
+    while (!window.shouldClose()) {
+        try window.update();
+    }
+}
